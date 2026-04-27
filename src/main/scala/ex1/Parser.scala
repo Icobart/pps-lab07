a31d016370ff2a10ef1d668d6d6d5dd1e2f2a2c0
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2026-04-27 12:03:05 +0200

    implemented parser extension for String scala type

--- src/main/scala/ex1/Parser.scala
+++ src/main/scala/ex1/Parser.scala
@@ -1,5 +1,7 @@
 package ex1
 
+import ex1.Parsers.charParser
+
 /** Consider the Parser example shown in previous lesson. Analogously to
   * NonEmpty, create a mixin NotTwoConsecutive, which adds the idea that one
   * cannot parse two consecutive elements which are equal. Use it (as a mixin)
@@ -14,7 +16,13 @@ abstract class Parser[T]:
     (seq forall parse) & end // note &, not &&
 
 object Parsers:
-  val todo = ??? // put the extensions here..
+  extension (string: String)
+    def charParser(): Parser[Char] =
+      new Parser[Char] {
+        override def parse(t: Char): Boolean =
+          string.contains(t)
+        override def end: Boolean = true
+      }
 class BasicParser(chars: Set[Char]) extends Parser[Char]:
   override def parse(t: Char): Boolean = chars.contains(t)
   override def end: Boolean = true
@@ -78,7 +86,7 @@ class NotTwoConsecutiveParser(chars: Set[Char])
   println(parserNTCNE.parseAll("XYYZ".toList)) // false
   println(parserNTCNE.parseAll("".toList)) // false
 
-  def sparser: Parser[Char] = ??? // "abc".charParser()
+  def sparser: Parser[Char] =  "abc".charParser()
   println(sparser.parseAll("aabc".toList)) // true
   println(sparser.parseAll("aabcdc".toList)) // false
   println(sparser.parseAll("".toList)) // true
